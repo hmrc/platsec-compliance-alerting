e2237611583ab620cd2284effc191f07fde89abd
jezd-hmrc <jez.daniels@digital.hmrc.gov.uk>
2021-06-15 14:48:58 +0100

    added slack mappings

---
+++ src/slack_notification_mappings.py
@@ -0,0 +1,20 @@
+from typing import Any, Dict, List, Set
+
+mapping = [
+    {
+        "slack_channel": "#team-infra-s3-alerts",
+        "buckets": [
+            "infra-prod-bucket-1",
+            "infra-bucket-2",
+            "infra-bucket-3",
+        ]
+    },
+    {
+        "slack_channel": "#team-infra-prod-alerts",
+        "buckets": [
+            "infra-prod-bucket-1",
+            "infra-prod-bucket-2",
+            "infra-prod-bucket-3",
+        ]
+    },
+]
