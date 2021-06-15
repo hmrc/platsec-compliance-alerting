71f0403b4bff0fb34f25a56e7d3f70d944a398a7
jezd-hmrc <jez.daniels@digital.hmrc.gov.uk>
2021-06-15 11:47:10 +0100

    some refactoring

--- src/notifications_filter.py
+++ src/notifications_filter.py
@@ -5,9 +5,4 @@ from src.notification import Notification
 
 class NotificationsFilter:
     def do_filter(self, notifications: Set[Notification], filters: List[Dict[str, str]]) -> Set[Notification]:
-        filtered = set()
-        undesired_buckets = [f["bucket"] for f in filters]
-        for notification in notifications:
-            if notification.bucket not in undesired_buckets:
-                filtered.add(notification)
-        return filtered
+        return {n for n in notifications if n.bucket not in [f["bucket"] for f in filters]}
--- tests/test_notifications_filter.py
+++ tests/test_notifications_filter.py
@@ -18,3 +18,18 @@ class TestNotificationsFilter(TestCase):
             Notification(bucket="private-bucket", findings=set()),
             Notification(bucket="another-bucket", findings=set())
         }, filtered)
+
+    def test_empty_filters(self) -> None:
+        notifications = {
+            Notification(bucket="private-bucket", findings=set()),
+            Notification(bucket="public-bucket", findings=set()),
+            Notification(bucket="another-bucket", findings=set()),
+            Notification(bucket="unencrypted-bucket", findings=set()),
+        }
+        filtered = NotificationsFilter().do_filter(notifications=notifications, filters=[])
+        self.assertEqual(notifications, filtered)
+
+    def test_empty_notifications(self) -> None:
+        filters = [{"bucket": "public-bucket"}, {"bucket": "unencrypted-bucket"}]
+        filtered = NotificationsFilter().do_filter(notifications=set(), filters=filters)
+        self.assertEqual(set(), filtered)
