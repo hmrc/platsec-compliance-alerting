7cc247d7bb7bb36b2b6c87db046b8091381b19a0
Antoine <achatain@users.noreply.github.com>
2021-06-21 18:16:38 +0100

    [PSEC-1219] Filter notifications out when they have no findings

--- src/notification_filter.py
+++ src/notification_filter.py
@@ -5,5 +5,6 @@ from src.config.notification_filter_config import NotificationFilterConfig
 
 
 class NotificationFilter:
-    def do_filter(self, notifications: Set[Notification], filters: List[NotificationFilterConfig]) -> Set[Notification]:
-        return {n for n in notifications if n.bucket not in [f.bucket for f in filters]}
+    @staticmethod
+    def do_filter(notifications: Set[Notification], filters: List[NotificationFilterConfig]) -> Set[Notification]:
+        return {n for n in notifications if n.findings and n.bucket not in [f.bucket for f in filters]}
--- tests/test_notification_filter.py
+++ tests/test_notification_filter.py
@@ -4,10 +4,10 @@ from src.data.notification import Notification
 from src.config.notification_filter_config import NotificationFilterConfig
 from src.notification_filter import NotificationFilter
 
-private_bucket = Notification(bucket="private-bucket")
-public_bucket = Notification(bucket="public-bucket")
+private_bucket = Notification(bucket="private-bucket", findings={"finding 1"})
+public_bucket = Notification(bucket="public-bucket", findings={"finding 2"})
 another_bucket = Notification(bucket="another-bucket")
-unencrypted_bucket = Notification(bucket="unencrypted-bucket")
+unencrypted_bucket = Notification(bucket="unencrypted-bucket", findings={"finding 3"})
 
 public_bucket_filter = NotificationFilterConfig("public-bucket")
 unencrypted_bucket_filter = NotificationFilterConfig("unencrypted-bucket")
@@ -17,10 +17,10 @@ class TestNotificationFilter(TestCase):
     def test_filter(self) -> None:
         notifications = {private_bucket, public_bucket, another_bucket, unencrypted_bucket}
         filters = [public_bucket_filter, unencrypted_bucket_filter]
-        self.assertEqual({private_bucket, another_bucket}, NotificationFilter().do_filter(notifications, filters))
+        self.assertEqual({private_bucket}, NotificationFilter().do_filter(notifications, filters))
 
     def test_empty_filters(self) -> None:
-        notifications = {private_bucket, public_bucket, another_bucket, unencrypted_bucket}
+        notifications = {private_bucket, public_bucket, unencrypted_bucket}
         self.assertEqual(notifications, NotificationFilter().do_filter(notifications=notifications, filters=[]))
 
     def test_empty_notifications(self) -> None:
