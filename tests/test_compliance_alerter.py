e435771852d9e408d663d5b4abb44abf6c1878b5
Antoine <achatain@users.noreply.github.com>
2021-08-31 14:20:07 +0100

    [PSEC-1312] GitHub compliance alerting integration test

--- tests/test_compliance_alerter.py
+++ tests/test_compliance_alerter.py
@@ -11,18 +11,20 @@ from moto import mock_s3, mock_ssm, mock_sts
 
 from src import compliance_alerter
 
+from tests.fixtures.github_compliance import github_report
 from tests.fixtures.s3_compliance_alerter import s3_report
 
 channel = "the-alerting-channel"
 config = "the_config_bucket"
 report = "the_report_bucket"
-report_key = "s3_audit"
-github_report_key = "github_admin_report"
+s3_key = "s3_audit"
+github_key = "github_audit"
 slack_api_url = "https://the-slack-api-url.com"
 slack_username_key = "the-slack-username-key"
 slack_token_key = "the-slack-token-key"
 
-event = {"Records": [{"eventVersion": "2.1", "s3": {"bucket": {"name": report}, "object": {"key": report_key}}}]}
+s3_event = {"Records": [{"eventVersion": "2.1", "s3": {"bucket": {"name": report}, "object": {"key": s3_key}}}]}
+github_event = {"Records": [{"eventVersion": "2.1", "s3": {"bucket": {"name": report}, "object": {"key": github_key}}}]}
 
 
 @mock_s3
@@ -37,9 +39,18 @@ class TestComplianceAlerter(TestCase):
         self._setup_ssm_parameters()
         self._mock_slack_notifier()
 
-    def test_compliance_alerter_main(self) -> None:
-        compliance_alerter.main(event)
+    def tearDown(self) -> None:
+        self._delete_bucket(report)
+        self._delete_bucket(config)
+        self._delete_ssm_parameters()
+
+    def test_compliance_alerter_main_s3_audit(self) -> None:
+        compliance_alerter.main(s3_event)
         self._assert_slack_message_sent("bad-bucket")
+
+    def test_compliance_alerter_main_github_audit(self) -> None:
+        compliance_alerter.main(github_event)
+        self._assert_slack_message_sent("bad-repo-no-signing")
 
     @staticmethod
     def _setup_environment() -> None:
@@ -54,8 +65,8 @@ class TestComplianceAlerter(TestCase):
                 "CONFIG_BUCKET": config,
                 "CONFIG_BUCKET_READ_ROLE": "the-config-bucket-read-role",
                 "REPORT_BUCKET_READ_ROLE": "the-report-bucket-read-role",
-                "S3_AUDIT_REPORT_KEY": report_key,
-                "GITHUB_AUDIT_REPORT_KEY": github_report_key,
+                "S3_AUDIT_REPORT_KEY": s3_key,
+                "GITHUB_AUDIT_REPORT_KEY": github_key,
                 "SLACK_API_URL": slack_api_url,
                 "SLACK_USERNAME_KEY": slack_username_key,
                 "SLACK_TOKEN_KEY": slack_token_key,
@@ -68,20 +79,37 @@ class TestComplianceAlerter(TestCase):
     def _setup_report_bucket() -> None:
         s3 = boto3.client("s3")
         s3.create_bucket(Bucket=report)
-        s3.put_object(Bucket=report, Key=report_key, Body=dumps(s3_report))
+        s3.put_object(Bucket=report, Key=s3_key, Body=dumps(s3_report))
+        s3.put_object(Bucket=report, Key=github_key, Body=dumps(github_report))
 
     @staticmethod
     def _setup_config_bucket() -> None:
         s3 = boto3.client("s3")
         s3.create_bucket(Bucket=config)
         s3.put_object(Bucket=config, Key="filters/a", Body=dumps([{"item": "mischievous-bucket", "reason": "because"}]))
-        s3.put_object(Bucket=config, Key="mappings/b", Body=dumps([{"channel": "alerts", "items": ["bad-bucket"]}]))
+        s3.put_object(Bucket=config, Key="filters/b", Body=dumps([{"item": "bad-repo-no-admin", "reason": "because"}]))
+        s3.put_object(Bucket=config, Key="mappings/a", Body=dumps([{"channel": "alerts", "items": ["bad-bucket"]}]))
+        s3.put_object(
+            Bucket=config, Key="mappings/b", Body=dumps([{"channel": "alerts", "items": ["bad-repo-no-signing"]}])
+        )
 
     @staticmethod
     def _setup_ssm_parameters() -> None:
         ssm = boto3.client("ssm")
         ssm.put_parameter(Name=slack_username_key, Value="the-slack-username", Type="SecureString")
         ssm.put_parameter(Name=slack_token_key, Value="the-slack-username", Type="SecureString")
+
+    @staticmethod
+    def _delete_ssm_parameters() -> None:
+        ssm = boto3.client("ssm")
+        ssm.delete_parameter(Name=slack_username_key)
+        ssm.delete_parameter(Name=slack_token_key)
+
+    @staticmethod
+    def _delete_bucket(bucket_name: str) -> None:
+        config_bucket = boto3.resource("s3").Bucket(bucket_name)
+        config_bucket.objects.all().delete()
+        config_bucket.delete()
 
     @staticmethod
     def _mock_slack_notifier() -> None:
