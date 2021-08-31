b099096845fab6e73e481a5bc297d43dd170e99b
Marti B <5116631+martibutler@users.noreply.github.com>
2021-08-31 12:48:47 +0100

    Update src/compliance/analyser_interface.py
    Co-authored-by: Antoine <achatain@users.noreply.github.com>

--- src/compliance/analyser_interface.py
+++ src/compliance/analyser_interface.py
@@ -7,4 +7,7 @@ from typing import Set
 class AnalyserInterface(ABC):
     @abstractmethod
     def analyse(self, audit: Audit) -> Set[Notification]:
-        pass
+        """
+        :param audit: an audit report
+        :return: set of notifications for findings in the audit report
+        """
