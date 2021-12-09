dfd63addebdf6c2bfea3e60b007a008917063fdd
Antoine <achatain@users.noreply.github.com>
2021-12-09 12:15:12 +0000

    [PSEC-1104] Prettify actions in detailed alerts

--- src/compliance/action_describer.py
+++ src/compliance/action_describer.py
@@ -31,10 +31,10 @@ class BriefActionDescriber(ActionDescriber):
 
 class DetailedActionDescriber(ActionDescriber):
     def describe(self, actions: Sequence[Action]) -> Sequence[str]:
-        return ", ".join(a.detailed_description for a in actions)
+        return "\n".join(a.detailed_description for a in actions)
 
     def describe_applied(self, actions: Sequence[Action]) -> Sequence[str]:
-        return ", ".join(a.detailed_description for a in actions if a.is_applied())
+        return "\n".join(a.detailed_description for a in actions if a.is_applied())
 
     def describe_failed(self, actions: Sequence[Action]) -> Sequence[str]:
-        return ", ".join(f"{a.detailed_description} (error: {a.reason})" for a in actions if a.has_failed())
+        return "\n".join(f"{a.detailed_description}\nerror: {a.reason}" for a in actions if a.has_failed())
