6f5e362556d9a741192bc762da52bdce4377c8ea
cob16 <public+github@cormacbrady.info>
2022-01-18 16:06:49 +0000

    include urls to failed builds

--- src/sns/codebuild.py
+++ src/sns/codebuild.py
@@ -1,3 +1,4 @@
+import urllib
 from typing import Dict, Any
 
 from src.data.account import Account
@@ -10,8 +11,28 @@ class CodeBuild:
     @staticmethod
     def create_finding(message: Dict[str, Any]) -> Findings:
         account = Account(identifier=message["account"])
+        region = message["region"]
         project_name = message["detail"]["project-name"]
         build_id = message["detail"]["build-id"]
         build_status = message["detail"]["build-status"]
-        finding = f"build {build_id} {build_status}"
-        return Findings(compliance_item_type="codebuild", account=account, item=project_name, findings={finding})
+        title = f"{project_name} {build_status}"
+        logs_url = message["detail"]["additional-information"]["logs"]["deep-link"]
+
+        codebuild_url = CodeBuild.generate_codebuild_url(
+            account_id=account.identifier, build_id=build_id, project_name=project_name, region=region
+        )
+
+        return Findings(
+            compliance_item_type="codebuild",
+            account=account,
+            item=title,
+            findings={f"url : {codebuild_url}", f"logs: {logs_url}"},
+        )
+
+    @staticmethod
+    def generate_codebuild_url(account_id: str, build_id: str, project_name: str, region: str) -> str:
+        build_url_suffix = urllib.parse.quote(build_id.split("build/")[-1])
+        return (
+            f"https://{region}.console.aws.amazon.com/codesuite/codebuild/"
+            f"{account_id}/projects/{project_name}/build/{build_url_suffix}/?region={region}"
+        )
