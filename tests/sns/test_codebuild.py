614434e8385724704a1ec3a6eaccff0dbca1c060
cob16 <public+github@cormacbrady.info>
2022-01-18 16:36:37 +0000

    convert urls to <a> tag

--- tests/sns/test_codebuild.py
+++ tests/sns/test_codebuild.py
@@ -12,20 +12,20 @@ def test_event_to_findings() -> None:
     assert finding.account.identifier == "123456789012"
     assert finding.compliance_item_type == "codebuild"
     assert finding.item == "ua-test FAILED"
-    assert len(finding.findings) == 2
+    assert len(finding.findings) == 1
 
     expected_logs_link = (
-        "logs: https://console.aws.amazon.com/cloudwatch/home"
+        "https://console.aws.amazon.com/cloudwatch/home"
         "?region=eu-west-2"
         "#logEvent:group=/aws/codebuild/ua-test;"
         "stream=7cf9f577-4069-4010-a467-f0640dfc0afb"
     )
 
-    expected_build_url = (
-        "url : https://eu-west-2.console.aws.amazon.com/codesuite/codebuild/123456789012/projects/"
+    expected_build_link = (
+        "https://eu-west-2.console.aws.amazon.com/codesuite/codebuild/123456789012/projects/"
         "ua-test/build/ua-test%3A7cf9f577-4069-4010-a467-f0640dfc0afb/?region=eu-west-2"
     )
-    assert finding.findings == frozenset({expected_build_url, expected_logs_link})
+    assert finding.findings == frozenset({f"<{expected_build_link}|build> <{expected_logs_link}|logs>"})
 
 
 def load_json_resource(filename: str) -> Any:
