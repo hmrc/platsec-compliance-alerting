596317fb618a309d35b8d43811bfbdce628cabfc
Tom Roffe <tom@altobyte.io>
2023-01-30 19:48:02 +0000

    Update compliance report alert on repo hasWikiEnabled status
     - added _has_wiki_enabled function to GithubCompliance Analyser class
     - updated _check_repository_rules with _has_wiki_enabled check
     - fixed some formatting in GithubCompliance class
     - added two new tests for _has_wiki_enabled function
     - updated existing fixtures to include new report field "hasWikiEnabled"
     - added new findings testcase
    
    Signed-off-by: Tom Roffe <tom@altobyte.io>

--- src/compliance/github_compliance.py
+++ src/compliance/github_compliance.py
@@ -7,7 +7,11 @@ from src.compliance.analyser import Analyser
 
 class GithubCompliance(Analyser):
     def analyse(self, audit: Audit) -> Set[Findings]:
-        return {self._check_repository_rules(repository) for repository in audit.report if not repository["isFork"]}
+        return {
+            self._check_repository_rules(repository)
+            for repository in audit.report
+            if not repository["isFork"]
+        }
 
     def _check_repository_rules(self, repository: Dict[str, Any]) -> Findings:
         findings = set()
@@ -18,6 +22,9 @@ class GithubCompliance(Analyser):
         if not self._is_admin_permission(repository):
             findings.add("repository should have admin permissions")
 
+        if self._has_wiki_enabled(repository):
+            findings.add("repository has wiki enabled")
+
         return Findings(
             description=f"<https://www.github.com/{repository['nameWithOwner']}|{repository['name']}>",
             compliance_item_type="github_repository",
@@ -26,7 +33,13 @@ class GithubCompliance(Analyser):
         )
 
     def _is_signed(self, repository: Dict[str, Any]) -> bool:
-        return True in [bpr["requiresCommitSignatures"] for bpr in repository["branchProtectionRules"]["nodes"]]
+        return True in [
+            bpr["requiresCommitSignatures"]
+            for bpr in repository["branchProtectionRules"]["nodes"]
+        ]
 
     def _is_admin_permission(self, repository: Dict[str, Any]) -> bool:
         return bool(repository["teamPermissions"] == "ADMIN")
+
+    def _has_wiki_enabled(self, repository: Dict[str, Any]) -> bool:
+        return repository["hasWikiEnabled"] == True
--- tests/compliance/test_github_compliance.py
+++ tests/compliance/test_github_compliance.py
@@ -12,14 +12,22 @@ class TestGithubCompliance(TestCase):
     def test_repository_is_signed(self) -> None:
         self.assertTrue(
             GithubCompliance(getLogger())._is_signed(
-                {"branchProtectionRules": {"nodes": [{"requiresCommitSignatures": True}]}}
+                {
+                    "branchProtectionRules": {
+                        "nodes": [{"requiresCommitSignatures": True}]
+                    }
+                }
             )
         )
 
     def test_repository_is_not_signed(self) -> None:
         self.assertFalse(
             GithubCompliance(getLogger())._is_signed(
-                {"branchProtectionRules": {"nodes": [{"requiresCommitSignatures": False}]}}
+                {
+                    "branchProtectionRules": {
+                        "nodes": [{"requiresCommitSignatures": False}]
+                    }
+                }
             )
         )
 
@@ -51,11 +59,29 @@ class TestGithubCompliance(TestCase):
             )
         )
 
+    def test_repository_has_wiki_enabled(self) -> None:
+        self.assertTrue(
+            GithubCompliance(getLogger())._has_wiki_enabled({"hasWikiEnabled": True})
+        )
+
+    def test_repository_has_wiki_disabled(self) -> None:
+        self.assertFalse(
+            GithubCompliance(getLogger())._has_wiki_enabled({"hasWikiEnabled": False})
+        )
+
     def test_repository_is_admin_permission(self) -> None:
-        self.assertTrue(GithubCompliance(getLogger())._is_admin_permission({"teamPermissions": "ADMIN"}))
+        self.assertTrue(
+            GithubCompliance(getLogger())._is_admin_permission(
+                {"teamPermissions": "ADMIN"}
+            )
+        )
 
     def test_repository_is_not_admin_permission(self) -> None:
-        self.assertFalse(GithubCompliance(getLogger())._is_admin_permission({"teamPermissions": "WRITE"}))
+        self.assertFalse(
+            GithubCompliance(getLogger())._is_admin_permission(
+                {"teamPermissions": "WRITE"}
+            )
+        )
 
     def test_check(self) -> None:
         audit = Audit(
@@ -87,10 +113,24 @@ class TestGithubCompliance(TestCase):
             findings(
                 account=None,
                 region_name=None,
+                description="<https://www.github.com/org/bad-repo-has-wiki-enabled|bad-repo-has-wiki-enabled>",
+                compliance_item_type="github_repository",
+                item="bad-repo-has-wiki-enabled",
+                findings={
+                    "repository has wiki enabled",
+                },
+            ),
+            findings(
+                account=None,
+                region_name=None,
                 description="<https://www.github.com/org/good-repo|good-repo>",
                 compliance_item_type="github_repository",
                 item="good-repo",
                 findings=set(),
             ),
         }
-        assert notifications == expected_findings
+        print(notifications)
+        print(len(notifications))
+        print(expected_findings)
+        print(len(expected_findings))
+        self.assertEqual(notifications, expected_findings)
