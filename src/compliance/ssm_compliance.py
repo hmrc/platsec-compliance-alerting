3e78c472a91bffdbf16aba0585210c6a728cd3fd
wunzeco <1915636+wunzeco@users.noreply.github.com>
2023-10-20 15:55:06 +0100

    PSEC-1953 fix style checker violations

--- src/compliance/ssm_compliance.py
+++ src/compliance/ssm_compliance.py
@@ -4,18 +4,16 @@ from src.data.account import Account
 
 from src.data.audit import Audit
 from src.data.findings import Findings
-from src.compliance.actionable_report_compliance import DetailedActionableReportCompliance
 from src.compliance.analyser import Analyser
 
 
 class SSMCompliance(Analyser):
-
     def __init__(self, logger: Logger):
         self.item_type = "ssm_document"
         super().__init__(logger, item_type=self.item_type)
 
     def analyse(self, audit: Audit) -> Set[Findings]:
-        findings: Findings = set()
+        findings = set()
         for sub_report in audit.report:
             for document in sub_report["results"]["documents"]:
                 if not document["compliant"]:
@@ -26,7 +24,7 @@ class SSMCompliance(Analyser):
                             region_name=sub_report["region"],
                             item=document["name"],
                             findings={f"compliant: {document['compliant']}"},
-                            description=f"SSM Document {document['name']} does not match the expected config", 
+                            description=f"SSM Document {document['name']} does not match the expected config",
                         )
                     )
         return findings
