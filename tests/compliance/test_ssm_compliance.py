9637095796b017956bd688be096c25af3731fb54
wunzeco <1915636+wunzeco@users.noreply.github.com>
2023-10-23 10:18:06 +0100

    PSEC-1953 improve findings messages to be more specific on non compliant config items

--- tests/compliance/test_ssm_compliance.py
+++ tests/compliance/test_ssm_compliance.py
@@ -1,7 +1,11 @@
 from logging import getLogger
 from src.compliance.ssm_compliance import SSMCompliance
 
-from tests.fixtures.ssm_compliance import ssm_audit_report
+from tests.fixtures.ssm_compliance import (
+    ssm_audit_report,
+    ssm_audit_report_non_compliant_s3_config_items,
+    ssm_audit_report_non_compliant_max_session_duration_shell_profile,
+)
 from tests.test_types_generator import account, create_audit, findings
 
 
@@ -17,6 +21,48 @@ finding_1 = findings(
 )
 
 
-def test_analyse_ssm_document_audit() -> None:
+def test_analyse_ssm_document_audit_compliant() -> None:
     audit = create_audit(type="audit_ssm_document.json", report=ssm_audit_report)
-    assert SSMCompliance(getLogger()).analyse(audit) == {finding_1}
+    assert SSMCompliance(getLogger()).analyse(audit) == {
+        findings(
+            account=acc_1,
+            compliance_item_type="ssm_document",
+            item="SSM-SessionManagerRunShell",
+            findings=set(),
+            description="SSM Document SSM-SessionManagerRunShell does not match the expected config",
+        )
+    }
+
+
+def test_analyse_ssm_document_audit_non_compliant_on_s3_config_items() -> None:
+    audit = create_audit(type="audit_ssm_document.json", report=ssm_audit_report_non_compliant_s3_config_items)
+    assert SSMCompliance(getLogger()).analyse(audit) == {
+        findings(
+            account=acc_1,
+            compliance_item_type="ssm_document",
+            item="SSM-SessionManagerRunShell",
+            findings={
+                "S3 bucket name should be mdtp-ssm-session-manager-audit-logs",
+                "S3 encryption should be enabled",
+            },
+            description="SSM Document SSM-SessionManagerRunShell does not match the expected config",
+        )
+    }
+
+
+def test_analyse_ssm_document_audit_non_compliant_on_session_duration_and_shell_profile() -> None:
+    audit = create_audit(
+        type="audit_ssm_document.json", report=ssm_audit_report_non_compliant_max_session_duration_shell_profile
+    )
+    assert SSMCompliance(getLogger()).analyse(audit) == {
+        findings(
+            account=acc_1,
+            compliance_item_type="ssm_document",
+            item="SSM-SessionManagerRunShell",
+            findings={
+                "maxSessionDuration should be less than or equal to 120 mins",
+                "shellProfile should match expected config",
+            },
+            description="SSM Document SSM-SessionManagerRunShell does not match the expected config",
+        )
+    }
--- tests/fixtures/ssm_compliance.py
+++ tests/fixtures/ssm_compliance.py
@@ -3,6 +3,78 @@ ssm_audit_report = [
         "account": {"identifier": "111222333444", "name": "account-1"},
         "region": "test-region-name",
         "description": "Audit SSM RunShell Document",
-        "results": {"documents": [{"name": "SSM-SessionManagerRunShell", "compliant": False}]},
+        "results": {
+            "documents": [
+                {
+                    "name": "SSM-SessionManagerRunShell",
+                    "compliancy": {
+                        "s3BucketName": {
+                            "compliant": True,
+                            "message": "S3 bucket name should be mdtp-ssm-session-manager-audit-logs",
+                        },
+                        "s3EncryptionEnabled": {"compliant": True, "message": "S3 encryption should be enabled"},
+                        "maxSessionDuration": {
+                            "compliant": True,
+                            "message": "maxSessionDuration should be less than or equal to 120 mins",
+                        },
+                        "shellProfile": {"compliant": True, "message": "shellProfile should match expected config"},
+                    },
+                }
+            ]
+        },
+    }
+]
+
+ssm_audit_report_non_compliant_s3_config_items = [
+    {
+        "account": {"identifier": "111222333444", "name": "account-1"},
+        "region": "test-region-name",
+        "description": "Audit SSM RunShell Document",
+        "results": {
+            "documents": [
+                {
+                    "name": "SSM-SessionManagerRunShell",
+                    "compliancy": {
+                        "s3BucketName": {
+                            "compliant": False,
+                            "message": "S3 bucket name should be mdtp-ssm-session-manager-audit-logs",
+                        },
+                        "s3EncryptionEnabled": {"compliant": False, "message": "S3 encryption should be enabled"},
+                        "maxSessionDuration": {
+                            "compliant": True,
+                            "message": "maxSessionDuration should be less than or equal to 120 mins",
+                        },
+                        "shellProfile": {"compliant": True, "message": "shellProfile should match expected config"},
+                    },
+                }
+            ]
+        },
+    }
+]
+
+ssm_audit_report_non_compliant_max_session_duration_shell_profile = [
+    {
+        "account": {"identifier": "111222333444", "name": "account-1"},
+        "region": "test-region-name",
+        "description": "Audit SSM RunShell Document",
+        "results": {
+            "documents": [
+                {
+                    "name": "SSM-SessionManagerRunShell",
+                    "compliancy": {
+                        "s3BucketName": {
+                            "compliant": True,
+                            "message": "S3 bucket name should be mdtp-ssm-session-manager-audit-logs",
+                        },
+                        "s3EncryptionEnabled": {"compliant": True, "message": "S3 encryption should be enabled"},
+                        "maxSessionDuration": {
+                            "compliant": False,
+                            "message": "maxSessionDuration should be less than or equal to 120 mins",
+                        },
+                        "shellProfile": {"compliant": False, "message": "shellProfile should match expected config"},
+                    },
+                }
+            ]
+        },
     }
 ]
