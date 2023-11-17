20f329953786007071bad9770b45dd7e2ccc74df
wunzeco <1915636+wunzeco@users.noreply.github.com>
2023-11-17 14:27:35 +0000

    PSEC-1966 refactor code to extend support for specific notifiers and payloads

--- src/notifiers/notifier.py
+++ src/notifiers/notifier.py
@@ -7,13 +7,13 @@ P = TypeVar("P")
 
 class Notifier(ABC, Generic[N, P]):
     @abstractmethod
-    def apply_filters(self, payloads: Set[P]) -> Set[P]:  # pragma: no cover
+    def apply_filters(self, payloads: Set[P]) -> Set[P]:
         pass
 
     @abstractmethod
-    def apply_mappings(self, payloads: Set[P]) -> List[N]:  # pragma: no cover
+    def apply_mappings(self, payloads: Set[P]) -> List[N]:
         pass
 
     @abstractmethod
-    def send(self, notifications: List[N]) -> None:  # pragma: no cover
+    def send(self, notifications: List[N]) -> None:
         pass
